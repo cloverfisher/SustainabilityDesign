ee3198bf6f2d4a8701001dd2751102c919d12600
cloverfisher <yuysyu@gmail.com>
2014-09-08 19:16:54 -0400

    add the Input indicators page and finish the add and delete method

--- src/mygui/InputIndicators.java
+++ src/mygui/InputIndicators.java
@@ -1,44 +1,36 @@
 package mygui;
 
-import guiplus.IndicatorListRender;
-
-import java.awt.BorderLayout;
-import java.awt.Container;
 import java.awt.EventQueue;
-import java.awt.ScrollPane;
-import java.awt.TextField;
-
-import javax.swing.JFrame;
-
-import java.awt.GridBagLayout;
-
-import javax.swing.JLabel;
-
 import java.awt.GridBagConstraints;
-
-import javax.swing.JScrollPane;
-
+import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.AbstractListModel;
 import javax.swing.DefaultListModel;
-import javax.swing.JList;
-import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.ListModel;
-import javax.swing.event.ListDataListener;
-import javax.swing.JScrollBar;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class InputIndicators {
 
 	private JFrame frame;
 //	private JTextField textField;
 	private JTextField addTextField;
-	DefaultListModel myListModel = new DefaultListModel();
+	DefaultListModel myListModel ;//= new DefaultListModel();
 	GridBagConstraints gbc_list = new GridBagConstraints();
-	JList list = new JList(myListModel);
+	JList jlist; //= new JList(myListModel);
+	 
+	List<String> list = new ArrayList<String>();  
+	private GridBagConstraints gbc_btnSave;
 	
 	
 	//String[] test = {"one","two"};
@@ -70,18 +62,22 @@ public class InputIndicators {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		myListModel = new DefaultListModel();
+		jlist = new JList(myListModel);
+		jlist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{75, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.weighty = 1.0;
 		gbc_lblNewLabel.weightx = 1.0;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 0;
@@ -89,14 +85,16 @@ public class InputIndicators {
 		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
 		
 		
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 0, 5);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 0;
-		gbc_textField.gridy = 1;
+//		GridBagConstraints gbc_textField = new GridBagConstraints();
+//		gbc_textField.insets = new Insets(0, 0, 0, 5);
+//		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+//		gbc_textField.gridx = 0;
+//		gbc_textField.gridy = 1;
 		
 		addTextField = new JTextField();
 		GridBagConstraints gbc_addTextField = new GridBagConstraints();
+		gbc_addTextField.weighty = 1.0;
+		gbc_addTextField.gridwidth = 2;
 		gbc_addTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_addTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_addTextField.gridx = 1;
@@ -105,14 +103,31 @@ public class InputIndicators {
 		addTextField.setColumns(10);
 		
 		JButton addButton = new JButton("add");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 2;
-		gbc_btnNewButton.gridy = 1;
-		frame.getContentPane().add(addButton, gbc_btnNewButton);
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.insets = new Insets(0, 0, 5, 0);
+		gbc_btnAdd.gridx = 3;
+		gbc_btnAdd.gridy = 1;
+		frame.getContentPane().add(addButton, gbc_btnAdd);
 		
-	
+		
+	//	jlist.setVisibleRowCount(-1);
+		
+	//	jlist.setSelectedIndex(0);
+	//	jlist.addListSelectionListener(InputIndicators.this);
+	//     jlist.addListSelectionListener(this);
 		//myListModel.addElement(obj);
+		jlist.addListSelectionListener(
+				new ListSelectionListener() {
+					
+					@Override
+					public void valueChanged(ListSelectionEvent e) {
+						// TODO Auto-generated method stub
+					//	jlist.setSelectedIndex(index);
+					//	if(jlist.getSelectedIndex()!=)
+					//	System.out.println(jlist.getSelectedIndex());
+					}
+				}
+					);
 		
 		addButton.addActionListener(new ActionListener() {
 			
@@ -120,36 +135,114 @@ public class InputIndicators {
 			public void actionPerformed(ActionEvent e) {
 				String tempText = new String();
 				tempText = addTextField.getText();
+				list.add(tempText);
 				System.out.println(tempText);
-				myListModel.addElement(tempText);
-			
-				list=new JList(myListModel);
+				myListModel.add(myListModel.getSize(),tempText);
+				
+			//	jlist=new JList(myListModel);
 				addTextField.setText("");
 		//		frame.getContentPane().add(list, gbc_list);
 			}
 		});
 		
+		JButton btnEconomic = new JButton("Economic");
+		GridBagConstraints gbc_btnEconomic = new GridBagConstraints();
+		gbc_btnEconomic.weighty = 1.0;
+		gbc_btnEconomic.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnEconomic.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEconomic.gridx = 0;
+		gbc_btnEconomic.gridy = 2;
+		frame.getContentPane().add(btnEconomic, gbc_btnEconomic);
+		
 	//	frame.getContentPane().add(textField, gbc_textField);
 	//	textField.setColumns(10);
 		
-		gbc_list.gridwidth = 2;
-		gbc_list.insets = new Insets(0, 0, 0, 5);
-		gbc_list.weightx = 4.0;
-		gbc_list.fill = GridBagConstraints.BOTH;
-		gbc_list.gridx = 3;
-		gbc_list.gridy = 2;
-//		list.setCellRenderer(new IndicatorListRender(list));
-	//	list.add
-		frame.getContentPane().add(list, gbc_list);
-		JScrollPane scrollPane = new JScrollPane(list);
+//		gbc_list.gridwidth = 2;
+//		gbc_list.insets = new Insets(0, 0, 0, 5);
+//		gbc_list.weightx = 4.0;
+//		gbc_list.fill = GridBagConstraints.BOTH;
+//		gbc_list.gridx = 3;
+//		gbc_list.gridy = 2;
+
+	//	frame.getContentPane().add(list, gbc_list);
+		JScrollPane scrollPane = new JScrollPane(jlist);
 		GridBagConstraints gbc_ScrollPane = new GridBagConstraints();
-		gbc_ScrollPane.gridwidth = 2;
+		gbc_ScrollPane.gridheight = 4;
+		gbc_ScrollPane.gridwidth = 3;
 		gbc_ScrollPane.insets = new Insets(0, 0, 5, 5);
 		gbc_ScrollPane.weightx = 4.0;
 		gbc_ScrollPane.fill = GridBagConstraints.BOTH;
 		gbc_ScrollPane.gridx = 1;
 		gbc_ScrollPane.gridy = 2;
 		frame.getContentPane().add(scrollPane, gbc_ScrollPane);
+		
+		JButton btnEnvironment = new JButton("Environment");
+		GridBagConstraints gbc_btnEnvironment = new GridBagConstraints();
+		gbc_btnEnvironment.weighty = 1.0;
+		gbc_btnEnvironment.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnEnvironment.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEnvironment.gridx = 0;
+		gbc_btnEnvironment.gridy = 3;
+		frame.getContentPane().add(btnEnvironment, gbc_btnEnvironment);
+		
+		JButton btnSocial = new JButton("Social");
+		GridBagConstraints gbc_btnSocial = new GridBagConstraints();
+		gbc_btnSocial.weighty = 1.0;
+		gbc_btnSocial.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnSocial.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSocial.gridx = 0;
+		gbc_btnSocial.gridy = 4;
+		frame.getContentPane().add(btnSocial, gbc_btnSocial);
+		
+		JButton btnExample = new JButton("Example");
+		GridBagConstraints gbc_btnExample = new GridBagConstraints();
+		gbc_btnExample.weighty = 1.0;
+		gbc_btnExample.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnExample.insets = new Insets(0, 0, 5, 5);
+		gbc_btnExample.gridx = 0;
+		gbc_btnExample.gridy = 5;
+		frame.getContentPane().add(btnExample, gbc_btnExample);
+		
+		JButton btnDeleteIndicator = new JButton("Delete");
+		btnDeleteIndicator.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO delete the select indicator in jlist&&list
+				
+				int tempIndex = jlist.getSelectedIndex();
+				if(tempIndex!=-1)
+				{
+					list.remove(tempIndex);
+					myListModel.remove(tempIndex);
+				}
+				System.out.println(tempIndex);
+			//	jlist.setSelectedIndex(2);
+			}
+		});
+		GridBagConstraints gbc_btnDeleteIndicator = new GridBagConstraints();
+		gbc_btnDeleteIndicator.weighty = 1.0;
+		gbc_btnDeleteIndicator.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnDeleteIndicator.insets = new Insets(0, 0, 0, 5);
+		gbc_btnDeleteIndicator.gridx = 1;
+		gbc_btnDeleteIndicator.gridy = 6;
+		frame.getContentPane().add(btnDeleteIndicator, gbc_btnDeleteIndicator);
+		
+		JButton btnSave = new JButton("Save");
+		GridBagConstraints gbc_btnSaveIndicator;
+		gbc_btnSave = new GridBagConstraints();
+		gbc_btnSave.weighty = 1.0;
+		gbc_btnSave.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnSave.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSave.gridx = 2;
+		gbc_btnSave.gridy = 6;
+		frame.getContentPane().add(btnSave, gbc_btnSave);
+		
+		JButton btnBack = new JButton("Back");
+		GridBagConstraints gbc_btnBack = new GridBagConstraints();
+		gbc_btnBack.weighty = 1.0;
+		gbc_btnBack.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnBack.gridx = 3;
+		gbc_btnBack.gridy = 6;
+		frame.getContentPane().add(btnBack, gbc_btnBack);
 //		list.setCellRenderer(new IndicatorListRender(list));
 //
 //	    Container contentPane = frame.getContentPane();
